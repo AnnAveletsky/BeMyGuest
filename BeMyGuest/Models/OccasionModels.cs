be793a382c31c8b274de1f43ab18f4dcc4e3d8bf
Аня Серба <annserba94@gmail.com>
2015-05-05 23:07:13 +0300

    Commit1All

---
+++ BeMyGuest/Models/OccasionModels.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+
+namespace BeMyGuest.Models
+{
+    public class OccasionModels:DbContext
+    {
+        public OccasionModels()
+            : base("aspnet-BeMyGuest-20150425021944")
+        {
+        }
+
+        public DbSet<Occasion> Occasions { get; set; }
+        
+    }
+    [Table("Occasion")]
+    public class Occasion
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int IdUserTraveler { get; set; }
+        public int IdUserHost { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+    }
+}
