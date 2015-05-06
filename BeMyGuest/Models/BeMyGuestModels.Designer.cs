2bc3bdbe02712877877b12a257e62c49d91cbde0
Aveletsky <ixus.van.axel@gmail.com>
2015-05-07 02:11:39 +0300

    not null in bd

--- BeMyGuest/Models/BeMyGuestModels.Designer.cs
+++ BeMyGuest/Models/BeMyGuestModels.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "E:\BeMyGuest\BeMyGuest\Models\BeMyGuestModels.edmx" включено. 
+﻿// Создание кода T4 для модели "F:\WEB\ASP.NET_MVC\BeMyGuest\BeMyGuest\Models\BeMyGuestModels.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
